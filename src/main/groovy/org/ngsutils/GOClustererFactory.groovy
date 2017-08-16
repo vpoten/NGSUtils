10e0f45c5656722477892b06277e5c6057179867
victor <vpoten@gmail.com>
2017-08-16 19:05:52 +0200

    initialize

---
+++ src/main/groovy/org/ngsutils/GOClustererFactory.groovy
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ngsutils
+
+import org.ngsutils.semantic.LinkedLifeDataFactory
+import org.ngsutils.ontology.GOManager
+import org.ngsutils.semantic.NGSDataResource
+import org.ngsutils.semantic.query.GeneQueryUtils
+import org.ngsutils.ontology.GOClusterer
+
+/**
+ *
+ * @author victor
+ */
+class GOClustererFactory {
+    
+    
+    public static void doGOCluster(workDir, taxonomyId='9606') {
+        // load semantic data
+        def graph = LinkedLifeDataFactory.loadRepository(LinkedLifeDataFactory.LIST_BASIC_GO, [taxonomyId], workDir)
+        def geneQuery = new GeneQueryUtils(graph)
+        
+        // prepare annotation
+        def goManager = new GOManager(graph)
+        def annotation = NGSDataResource.create(graph)
+        annotation.load(taxonomyId)
+        
+        // TODO
+        
+        def data = []  // List<URI>
+            
+        GOClusterer(graph, workDir, taxonomyId,  data)
+    }
+}
