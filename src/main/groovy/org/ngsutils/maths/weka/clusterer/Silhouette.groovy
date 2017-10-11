bd2207810e30d6567792639065c06f8a1d10f0a9
victor <vpoten@gmail.com>
2017-10-11 12:15:42 +0200

    improve similarity calc

--- src/main/groovy/org/ngsutils/maths/weka/clusterer/Silhouette.groovy
+++ src/main/groovy/org/ngsutils/maths/weka/clusterer/Silhouette.groovy
@@ -12,30 +12,45 @@ package org.ngsutils.maths.weka.clusterer
  */
 class Silhouette {
     
-    double [] overallCoeff
-    double [] intraAvgDist
+    double [] silhCoeff
+    double [] intraAvgDist // intra cluster average distance
+    double [] minAvgExtDist // minimun average distance to points in other clusters
+    double overall
+    double [] clusterOverall
     
     
     public Silhouette(AbstractFuzzyClusterer clusterer) {
         def clustering = clusterer.getClustering()
         
-        // calc intra cluster average distance for each point
-        intraAvgDist = new double [clusterer.numOfClusters]
-        
+        // calc intra cluster average distance for each point and
+        // calc minimun average distance to points in other clusters
+        silhCoeff = new double [clusterer.numInstances]
+        intraAvgDist = new double [clusterer.numInstances]
+        minAvgExtDist = new double [clusterer.numInstances]
         
         for(cluster in clustering) {
             def denom = 1.0/((double)cluster.size())
+            def others = clustering.findAll{it != cluster}
+            
             for(index in cluster) {
                 intraAvgDist[index] = denom * cluster.sum{clusterer.distanceByIndex(index, it)}
+                
+                minAvgExtDist[index] = others.collect{ cluster2 ->
+                    if(cluster2.isEmpty()) {return 0.0d}
+                    cluster2.sum{clusterer.distanceByIndex(index, it)} / ((double)cluster2.size())
+                }.min()
+                
+                silhCoeff[index] = (minAvgExtDist[index] - intraAvgDist[index]) /
+                    Math.max(minAvgExtDist[index], intraAvgDist[index])
             }
         }
         
-        // calc the minimun distance to points in other clusters
-        def minExtDist = new double [clusterer.numOfClusters]
-        // TODO
-        
-        
         // calc overall coefficient (average of all points)
+        overall = (silhCoeff as List).sum() / ((double)clusterer.numInstances)
+        
+        clusterOverall = clustering.collect{ cluster ->
+            cluster.sum{silhCoeff[it]} / ((double)cluster.size())
+        } as double []
     }	
 }
 
--- src/test/groovy/org/ngsutils/maths/weka/clusterer/SilhouetteTest.java
+++ src/test/groovy/org/ngsutils/maths/weka/clusterer/SilhouetteTest.java
@@ -42,7 +42,7 @@ public class SilhouetteTest {
         String file = "/home/victor/software/weka-3-6-9/data/iris.2D.arff";
         
         // create a 'good' cluster
-        String [] options = new String [] {"-C","3","-lambda","10","-gamma","0.01","-K","0"};
+        String [] options = new String [] {"-C","3","-lambda","10","-gamma","0.01"};
         AbstractFuzzyClusterer clusterer =
                 (AbstractFuzzyClusterer) CentralClustererUtils.runClusterer(file, true, CentralClustererUtils.getCLUST_KPCM(), options);
         assertNotNull(clusterer);
@@ -50,12 +50,23 @@ public class SilhouetteTest {
         Silhouette scc1 = new Silhouette(clusterer);
                 
         // create a 'bad' cluster
-        options = new String [] {"-C","3","-lambda","5","-gamma","0.01","-K","0"};
+        options = new String [] {"-C","3","-lambda","8","-gamma","0.01"};
         clusterer =
                 (AbstractFuzzyClusterer) CentralClustererUtils.runClusterer(file, true, CentralClustererUtils.getCLUST_KPCM(), options);
         
         Silhouette scc2 = new Silhouette(clusterer);
         
-        // TODO <----- scc1 should be better than scc2
+        // scc1 should be better than scc2
+        assertTrue(scc1.getOverall() > scc2.getOverall());
+        
+        // create another 'bad' cluster
+        options = new String [] {"-C","3","-lambda","2","-gamma","0.01"};
+        clusterer =
+                (AbstractFuzzyClusterer) CentralClustererUtils.runClusterer(file, true, CentralClustererUtils.getCLUST_KPCM(), options);
+        
+        Silhouette scc3 = new Silhouette(clusterer);
+        
+        // scc1 should be better than scc3
+        assertTrue(scc1.getOverall() > scc3.getOverall());
     }
 }
